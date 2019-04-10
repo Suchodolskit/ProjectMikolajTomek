33e32c525dabd28c20b0b49080c272acaeca045e
suchodolskit <tomaszsuchodolski1997@wp.pl>
2019-04-10 12:23:06 +0200

    Add singing features

---
+++ app/src/main/java/com/example/project_mikolaj_tomek/FirebaseAuthHelper.java
@@ -0,0 +1,69 @@
+package com.example.project_mikolaj_tomek;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class FirebaseAuthHelper {
+    private static final String TAG = "FirebaseAuthHelper";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private AppCompatActivity activity;
+
+    public FirebaseAuthHelper(FirebaseAuth mAuth, FirebaseAuth.AuthStateListener mAuthListener, AppCompatActivity activity) {
+        this.mAuth = mAuth;
+        this.mAuthListener = mAuthListener;
+        this.activity = activity;
+    }
+
+
+    private boolean returnSignInWithEmailResult;
+    public boolean SignInWithEmail(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            returnSignInWithEmailResult = false;
+                        }
+                        else{
+                            returnSignInWithEmailResult = true;
+                        }
+                    }
+                });
+        return returnSignInWithEmailResult;
+    }
+
+    private boolean returnSingUpWithEmailResult;
+    public boolean SignUpWithEmail(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.w(TAG, "creation successful");
+                            returnSingUpWithEmailResult = true;
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            returnSingUpWithEmailResult = false;
+                        }
+                    }
+
+                    // ...
+                });
+        return returnSingUpWithEmailResult;
+    }
+
+    public void SignOut() {
+        mAuth.signOut();
+    }
+}
--- app/src/main/java/com/example/project_mikolaj_tomek/MainActivity.java
+++ app/src/main/java/com/example/project_mikolaj_tomek/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.project_mikolaj_tomek;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private EditText mLoginText;
+    private EditText mPasswordText;
+
+    private Button mSubmitLoginInButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuthHelper authHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +49,48 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mLoginText = findViewById(R.id.logInText);
+        mPasswordText = findViewById(R.id.passwordText);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null) {
+                    Toast.makeText(MainActivity.this,"You are singed id :) ",Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+        authHelper = new FirebaseAuthHelper(mAuth,mAuthListener,this);
+
+        mSubmitLoginInButton = findViewById(R.id.submitSingInButton);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startLogIn() {
+        String login = mLoginText.getText().toString();
+        String password = mPasswordText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(login,password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"You cannot sign in with this email and password",Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +112,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void signOut(View view) {
+        authHelper.SignOut();
+    }
+
+    public void SingIn(View view) {
+        authHelper.SignInWithEmail(mLoginText.getText().toString(),mPasswordText.getText().toString());
+    }
+
+    public void signUp(View view) {
+        authHelper.SignUpWithEmail(mLoginText.getText().toString(),mPasswordText.getText().toString());
     }
 }
